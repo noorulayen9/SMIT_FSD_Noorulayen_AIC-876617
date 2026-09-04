618afacc5553c899f824aae25adbba12d4094007
noorulayen9 <noorulayen99@gmail.com>
2026-09-04 21:24:11 +0500

    Add files via upload

---
+++ week6/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+
+function App() {
+ var userName = "NOOR UL AYEN";
+ userName = "AROOJ";
+console.log("var:", userName);
+
+  
+
+  return (
+    <>
+     <h1>
+      JavaScript + React Basics
+      </h1>
+     Hello World!
+     <h2>
+      Variable concept in React
+      </h2>
+      <p>
+      Name: {userName}
+      </p>
+    
+
+
+
+
+
+
+
+
+
+
+
+    </>
+  )
+}
+
+export default App
